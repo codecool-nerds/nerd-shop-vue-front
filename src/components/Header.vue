631b45893d94498b4f1cc8a47543be18ba6e23bf
Adrian <radian.ost@gmail.com>
2019-03-20 13:59:14 +0100

    add header with logo, search bar and user avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,50 @@
-<template>
-  <div>
-    <h1>Header</h1>
-  </div>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-toolbar>
+    <v-toolbar-title class="ml-1 mr-3">Nerd Shop</v-toolbar-title>
+    <v-text-field
+            class="mx-4"
+            v-model="searchText"
+            placeholder="Search on the page"
+            append-outer-icon="search"
+            @click:append-outer="search"
+            @keyup="searchIfEnterPressed"
+            single-line
+    ></v-text-field>
+    <v-menu offset-y>
+      <template v-slot:activator="{on}">
+        <v-btn v-on="on" flat>
+          <v-avatar size="36px" class="mx-2">
+            <img src="https://picsum.photos/300/300?random">
+          </v-avatar>
+          Harry Potter
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title>Logout</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        data: () => ({
+            searchText: '',
+            drawer: null
+        }),
+        methods: {
+          search() {
+            this.searchText = '';
+          },
+          searchIfEnterPressed(event) {
+            if (event.keyCode == 13) {
+                this.search();
+            }
+          }
+        }
     }
 </script>
 
