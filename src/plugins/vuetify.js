01ebeb081c5bc066d4823c08761d33c81cecd979
Adrian <radian.ost@gmail.com>
2019-03-27 14:52:21 +0100

    change color theme from violet to blue

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,12 +5,12 @@ import 'vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   iconfont: 'md' || 'fa',
   theme: {
-    primary: '#4a148c',
-    secondary: '#7c43bd',
-    accent: '#ffb300',
-    error: '#d32f2f',
+    primary: '#003c8f',
+    secondary: '#1565c0',
+    accent: '#ffc400',
+    error: '#eb6100',
     warning: '#ffeb3b',
-    info: '#1976d2',
+    info: '#78909c',
     success: '#4caf50'
   }
 })
